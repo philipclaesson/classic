ab1539a776757df5755878f9adb25e730cde0f80
Philip Claesson <philip.claesson@northvolt.com>
2024-10-31 22:02:48 +0100

    new desktop studies

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ function render() {
     marker.bindPopup(barHTLM(bar)).openPopup();
   });
 
-  map.setView([59.313964, 18.070678], 13);
+  map.setView([59.313964, 18.070678], 6);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -130,5 +130,69 @@ const bars: Bar[] = [
     rating: 5,
     link: "https://maps.app.goo.gl/K2YTqXioFWZ5vrL77",
     coordinates: [59.336508867924756, 18.05397056988638],
+  },
+  {
+    name: "Dansken",
+    price: 56,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/nsajjzThR4VTw2Bd8",
+    coordinates: [57.69939979958528, 11.950095640988183],
+  },
+  {
+    name: "Kitchen 961",
+    price: 65,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/vxveKsEeGPptaSiq8",
+    coordinates: [55.606993991964416, 13.023420763334213],
+  },
+  {
+    name: "Wollmar",
+    price: 82,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/GxZbPytDyQPu3nz37",
+    coordinates: [59.31706806662487, 18.062508971738623],
+  },
+  {
+    name: "JP's Bar & Restaurant",
+    price: 74,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/u5cP1V69Vc7g9nmy7",
+    coordinates: [55.594584738316826, 12.984402631462256],
+  },
+  {
+    name: "The Bishops Arms - Umeå",
+    price: 96,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/9cCyyApYAGBMpryZ7",
+    coordinates: [63.82613832957854, 20.260176878958543],
+  },
+  {
+    name: "Restaurang Utsikten",
+    price: 75,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/LMLUvNw1VJ5tQH3K7",
+    coordinates: [56.04952406844887, 12.687655758124254],
+  },
+  {
+    name: "Telegrafen",
+    price: 79,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/K2BScQq4Cq2L35Qu7",
+    coordinates: [56.04829615313822, 12.694425598599004],
+  },
+  {
+    name: "Star",
+    price: 82,
+    type: "Fat",
+    rating: 5,
+    link: "https://maps.app.goo.gl/QKNeBgnxzfKRVsw5A",
+    coordinates: [55.593070698866384, 13.006663613926827],
   },
 ];
